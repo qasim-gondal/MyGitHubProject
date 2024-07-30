c8367ffc2c32a499af67b0d59737f61600a170cc
qasim-gondal <qgondal48@gmail.com>
2024-07-31 00:40:21 +0500

    Initial Commit

---
+++ MyGitHubProject/MyGitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGitHubProjectApp.swift
+//  MyGitHubProject
+//
+//  Created by Muhammad Qasim on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyGitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
